42e4d17fc3e9f387b9c80db4cce030be9b9e04b7
rasel <russelroy15@gmail.com>
2022-01-20 16:36:02 +0600

    WIP on Redux fundamental

--- src/reducer.js
+++ src/reducer.js
@@ -2,17 +2,19 @@
 let lastId = 0;
 
 function reducer(state = [], action) {
-  if (action.type === "bugAdded")
-    return [
-      ...state,
-      {
-        id: ++lastId,
-        description: action.payload.description,
-        resolved: false,
-      },
-    ];
-  else if (action.type === "bugRemoved")
-    return state.filter((bug) => bug.id !== action.payload.id);
-
-  return state;
+  switch (action.type) {
+    case "bugAdd":
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case "bugRemoved":
+      return state.filter((bug) => bug.id !== action.payload.id);
+    default:
+      return state;
+  }
 }
