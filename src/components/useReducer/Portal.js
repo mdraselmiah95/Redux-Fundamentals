650b3eba0a5478c13d59f1c6bbb037808b2f6a87
mdraselmiah95 <russelroy15@gmail.com>
2022-04-11 20:04:06 +0600

    WIP on Redux Fundamental project

--- src/components/useReducer/Portal.js
+++ src/components/useReducer/Portal.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+import { portalState } from "../../store/portalStore";
+import { portalReducer } from "../reducers/portalReducer";
 
 const Portal = () => {
+  const [state, dispatch] = useReducer(portalReducer, portalState);
+  const [name, setName] = useState("");
   return (
-    <div>
+    <div style={{ marginLeft: "10%" }}>
       <h1>This is Portal.</h1>
+      <h3>Has Patients:{state.patients.length}</h3>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <button>Add</button>
     </div>
   );
 };
