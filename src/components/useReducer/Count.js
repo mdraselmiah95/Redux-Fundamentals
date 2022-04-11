36f9973f7010c91927b6fa7d6045acfb91a76a02
mdraselmiah95 <russelroy15@gmail.com>
2022-04-11 19:18:26 +0600

    WIP on Redux Fundamental project

--- src/components/useReducer/Count.js
+++ src/components/useReducer/Count.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      break;
+
+    default:
+      break;
+  }
+};
 
 const Count = () => {
+  const [state, dispatch] = useReducer(countReducer);
   return (
     <div>
       <h1>rr</h1>
